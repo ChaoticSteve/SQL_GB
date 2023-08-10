1f34a6fc95e4b83b01494ae59a1e998ba8d2ed6e
Pavel Tyunin <91519022+ChaoticSteve@users.noreply.github.com>
2023-08-10 16:28:32 +0600

    Update task1.sql

--- Tyunin_Pavel_dz1/task1.sql
+++ Tyunin_Pavel_dz1/task1.sql
@@ -8,9 +8,9 @@ CREATE TABLE MobilePhones(
 INSERT INTO MobilePhones (id, product_name, manufacturer, product_count, price) VALUES
     (1, "IPhone X", "Apple", 3, 76000),
     (2, "IPhone 8", "Apple", 2, 51000),
-    (1, "Galaxy S9", "Samsung", 2, 56000),
-    (1, "Galaxy S8", "Samsung", 1, 41000),
-    (1, "P20 Pro", "Huawei", 5, 360000);
+    (3, "Galaxy S9", "Samsung", 2, 56000),
+    (4, "Galaxy S8", "Samsung", 1, 41000),
+    (5, "P20 Pro", "Huawei", 5, 360000);
 
 SELECT product_name, manufacturer, price
 FROM MobilePhones
